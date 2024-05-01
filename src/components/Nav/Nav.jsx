b5b7ef234b855df395dc481b80cbd4b81d63f4e5
Gaston <gastonmonzon3@gmail.com>
2024-05-01 15:12:47 -0300

    Add Responsive Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,18 +1,22 @@
 import './Nav.css';
 import { useEffect, useState } from 'react';
+import menuIcon from '../../assets/icons/menu-icon.svg';
 
-export default function Nav({ textContent, isEnglish, handleLanguageChange }) {
+export default function Nav({ textContent, isEnglish, handleLanguageChange, viewportWidth }) {
   const [isScrollHome, setIsScrollHome] = useState(false);
   const [isScrollAbout, setIsScrollAbout] = useState(false);
   const [isScrollPortfolio, setIsScrollPortfolio] = useState(false);
   const [isScrollSkills, setIsScrollSkills] = useState(false);
+  const [isScrollDocuments, setIsScrollDocuments] = useState(false);
   const [isScrollContact, setIsScrollContact] = useState(false);
+  const [isMenuFocused, setIsMenuFocused] = useState(false);
   const navButtons = [
     { id: 1, name: textContent.homeButton, href: '#headline', isScrolled: isScrollHome },
     { id: 2, name: textContent.aboutButton, href: '#about', isScrolled: isScrollAbout },
-    { id: 4, name: textContent.skillsButton, href: '#skills', isScrolled: isScrollSkills },
     { id: 3, name: textContent.portfolioButton, href: '#portfolio', isScrolled: isScrollPortfolio },
-    { id: 5, name: textContent.contactButton, href: '#contact', isScrolled: isScrollContact },
+    { id: 4, name: textContent.skillsButton, href: '#skills', isScrolled: isScrollSkills },
+    { id: 5, name: textContent.documentsButton, href: '#documents', isScrolled: isScrollDocuments },
+    { id: 6, name: textContent.contactButton, href: '#contact', isScrolled: isScrollContact },
   ];
 
   useEffect(() => {
@@ -69,6 +73,19 @@ export default function Nav({ textContent, isEnglish, handleLanguageChange }) {
 
   useEffect(() => {
     const handleScroll = () => {
+      const targetDiv = document.getElementById('documents');
+      const rect = targetDiv.getBoundingClientRect();
+      const isInView = rect.top <= 50 && rect.bottom >= 50;
+      setIsScrollDocuments(isInView);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
       const targetDiv = document.getElementById('contact');
       const rect = targetDiv.getBoundingClientRect();
       const isInView = rect.top <= 50 && rect.bottom >= 0;
@@ -80,21 +97,53 @@ export default function Nav({ textContent, isEnglish, handleLanguageChange }) {
     };
   }, []);
 
+  const handleOpenMenu = () => {
+    setIsMenuFocused(!isMenuFocused);
+  }
+  const handleMenuBlur = () => {
+    setIsMenuFocused(false);
+  }
+
   return (
-    <nav id='nav'>
-    <h2>Gaston Monzon</h2>
-    <div>
-      {navButtons.map((button) => {
-        return (
-          <button key={button.id} className={button.isScrolled ? 'button-scrolled' : ''} >
-            <a href={button.href}>{button.name}</a>
+    <>
+      <nav id='nav'>
+        <h2>Gaston Monzon</h2>
+        {viewportWidth < 900 &&
+          <button
+            className='menu-button'
+            onClick={handleOpenMenu}
+            onBlur={() => setTimeout(handleMenuBlur, 100)}
+          >
+            <img src={menuIcon} alt='Menu Icon' />
+          </button>}
+        {viewportWidth > 900 ?
+          <div>
+            {navButtons.map((button) => {
+              return (
+                <button key={button.id} className={button.isScrolled ? 'button-scrolled' : ''} >
+                  <a href={button.href}>{button.name}</a>
+                </button>
+              )
+            })}
+            <button onClick={handleLanguageChange} >
+              {isEnglish ? 'Español' : 'English'}
+            </button>
+          </div>
+          : null}
+      </nav>
+      {isMenuFocused &&
+        <div className='menu' >
+          {navButtons.map((button) => {
+            return (
+              <button key={button.id} className={button.isScrolled ? 'button-scrolled' : ''} >
+                <a href={button.href}>{button.name}</a>
+              </button>
+            )
+          })}
+          <button onClick={handleLanguageChange} >
+            {isEnglish ? 'Español' : 'English'}
           </button>
-        )
-      })}
-      <button onClick={handleLanguageChange} >
-        {isEnglish ? 'Español' : 'English'}
-      </button>
-    </div>
-  </nav>
+        </div>}
+    </>
   )
 }
